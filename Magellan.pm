2aca0b1f6fda7fad418ae47b1b626bed9b3637da
Peter Banik <peterzen@cpan.org>
2003-05-20 18:47:37 +0000

    Import of PETERZEN/GPS-Magellan-0.6 from CPAN.
    gitpan-cpan-distribution: GPS-Magellan
    gitpan-cpan-version:      0.6
    gitpan-cpan-path:         PETERZEN/GPS-Magellan-0.6.tar.gz
    gitpan-cpan-author:       PETERZEN
    gitpan-cpan-maturity:     released

--- Magellan.pm
+++ Magellan.pm
@@ -8,7 +8,7 @@ use GPS::Magellan::Coord;
 
 use vars qw($AUTOLOAD);
 
-our $VERSION = '0.5';
+our $VERSION = '0.6';
 
 sub new {
     my $proto = shift;
@@ -42,7 +42,7 @@ sub getPoints {
     my $self = shift;
     my $cmd = shift or die "getPoint( WAYPOINT | TRACKLOG )\n";
 
-    my @messages = $self->_command($cmd);
+    my @messages = $self->command($cmd);
     my @coords = (); 
     foreach my $msg (@messages){
         my $wpt = GPS::Magellan::Coord->new($msg->DATA);
@@ -51,13 +51,13 @@ sub getPoints {
     return @coords;
 }
 
-sub _command {
+sub command {
     my $self = shift;
     my $cmd = shift;
 
-    die "_command() needs cmd" unless $cmd;
+    die "command() needs cmd" unless $cmd;
 
-    return $self->__command($cmd) unless $self->RUN_OFFLINE;
+    return $self->_command($cmd) unless $self->RUN_OFFLINE;
 
     my $data_file = "test-data/$cmd";
 
@@ -72,25 +72,11 @@ sub _command {
         $_->DATA($data);
     } @result;
     
-
-# Record responses to file
-#
-#    open(DATA, ">$data_file") or die "cannot open $data_file\n";
-#
-#    my @result = $self->__command($cmd);
-#
-#    foreach my $line (@result) { 
-#        next unless $line;
-#        print DATA "$line\n";
-#    }
-#
-#    close(DATA);
-
     return @result;
 
 }
 
-sub __command {
+sub _command {
     my $self = shift;
     my $cmd = shift;
 
@@ -107,8 +93,6 @@ sub __command {
 
         my $msg = GPS::Magellan::Message->new($raw_msg);
         
-        # print $msg->_dump;
-
         if($msg->COMMAND eq 'CMD'){
             last if $msg->DATA eq 'END';
         }
@@ -173,10 +157,6 @@ sub DESTROY {
 require XSLoader;
 XSLoader::load('GPS::Magellan', $VERSION);
 
-
-
-
-
 1;
 
 __END__
@@ -187,6 +167,32 @@ GPS::Magellan - Module for communicating with Magellan receivers
 
 =head1 SYNOPSIS
 
+ GPS::Magellan::OpenPort('/dev/ttyS0');
+
+ $gps = GPS::Magellan->new(
+     port => '/dev/ttyS0'
+ );
+
+ # Download waypoints
+ @waypoints = $gps->getPoints('WAYPOINT');
+
+ foreach $coord (@waypoints){
+    printf("longitude: %s, latitude: %s\n", $coord->longitude, $coord->latitude);
+ }
+
+ # Download trackpoints
+ @trackpoints = $gps->getPoints('TRACKLOG');
+
+ foreach $coord (@trackpoints){
+    printf("longitude: %s, latitude: %s\n", $coord->longitude, $coord->latitude);
+ }
+
+ $file = GPS::Magellan::File::Way_Txt->new(
+    coords => \@waypoints
+ );
+ 
+ print $file->as_string();
+ 
 =cut
 
 =head1 DESCRIPTION
@@ -200,17 +206,31 @@ test suite for example.
 
 =cut
 
-=item new ( )
+=item new ( port => SERIAL_DEVICE )
 
+Instantiates a new GPS::Magellan object.  SERIAL_DEVICE specifies which port
+if the receiver connected to.
 
 =cut
 
 
+=item getPoints( 'WAYPOINT' | 'TRACKLOG' )
+
+Downloads coordinates of the specified type from the receiver.
+
+Returns: an array of L<GPS::Magellan::Coord> objects.
+
+=cut
+
+=item command( COMMAND )
+
+Send arbitrary command to the receiver.
+
+Returns: the receiver's response as an array of L<GPS::Magellan::Message> objects.
+
+=cut
+
 =back 4
-
-=head1 PREREQUISITES
-
-L<>
 
 =head1 AUTHOR
 
@@ -218,14 +238,20 @@ Peter Banik E<lt>peter@login-fo.netE<gt>
 
 =head1 SEE ALSO
 
-L<GPS::Magellan>
+L<GPS::Magellan::Coord>
+L<GPS::Magellan::Message>
+L<GPS::Magellan::File>
 
 =head1 VERSION
 
-$Id: Magellan.pm,v 1.1.1.1.4.8 2003/05/17 01:32:14 peter Exp $
+$Id: Magellan.pm,v 1.1.1.1.4.9 2003/05/20 18:39:15 peter Exp $
 
 =head1 BUGS
 
+ Missing coordinate upload feature.
+ Needs more documentation.
+ Instead of GPS::Magellan::Coord, it should make use of a generic coordinate class.
+ 
 Please report bugs to the author.
 
 =head1 COPYRIGHT
